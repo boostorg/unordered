cfe4c26f9907bd9e84aac3cfb1e989c76f5d5a45
Daniel James <daniel@calamity.org.uk>
2017-04-23 09:31:18 +0100

    Explicitly write out boost::tuple template arguments
    GCC 4.6 doesn't support using variadic arguments for a fixed length template.
    There's a config macro for this, but might as well use the same code
    everywhere.

--- include/boost/unordered/detail/implementation.hpp
+++ include/boost/unordered/detail/implementation.hpp
@@ -1470,8 +1470,10 @@ inline void construct_from_args(
 
 template <typename A0> struct detect_boost_tuple
 {
-    template <typename... T0>
-    static choice1::type test(choice1, boost::tuple<T0...> const&);
+    template <typename T0, typename T1, typename T2, typename T3, typename T4,
+        typename T5, typename T6, typename T7, typename T8, typename T9>
+    static choice1::type test(
+        choice1, boost::tuple<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> const&);
 
     static choice2::type test(choice2, ...);
 
