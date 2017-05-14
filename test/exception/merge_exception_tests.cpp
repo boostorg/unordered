dc611fc8282024b95bcd1de6963a7805adb5a1ad
Daniel James <daniel@calamity.org.uk>
2017-05-14 02:52:59 +0100

    Run fewer test in merge_exception_tests
    It's timing out on some platforms because it's too slow. This reduces
    the tag combinations for 9 to 3, which should reduce the execution time
    by a third. Also slightly reduces the count combinations, but that won't
    make much of a difference really - the slowest tests are still there
    (when the right hand side is large).

--- test/exception/merge_exception_tests.cpp
+++ test/exception/merge_exception_tests.cpp
@@ -30,10 +30,13 @@ template <typename T1, typename T2> void merge_exception_test(T1 x, T2 y)
 }
 
 template <typename T1, typename T2>
-void merge_exception_test(T1 const*, T2 const*, std::size_t count1,
-    std::size_t count2, int tag1, int tag2, test::random_generator gen1,
-    test::random_generator gen2)
+void merge_exception_test(T1 const*, T2 const*, std::size_t count12, int tag12,
+    test::random_generator gen1, test::random_generator gen2)
 {
+    std::size_t count1 = count12 / 256;
+    std::size_t count2 = count12 % 256;
+    int tag1 = tag12 / 256;
+    int tag2 = tag12 % 256;
     test::random_values<T1> v1(count1, gen1);
     test::random_values<T2> v2(count2, gen2);
     T1 x(v1.begin(), v1.end(), 0, test::exception::hash(tag1),
@@ -65,20 +68,16 @@ using test::limited_range;
 UNORDERED_MULTI_TEST(set_merge, merge_exception_test,
     ((test_set_)(test_multiset_))
     ((test_set_)(test_multiset_))
-    ((0)(10)(100))
-    ((0)(10)(100))
-    ((0)(1)(2))
-    ((0)(1)(2))
+    ((0x0000)(0x6400)(0x0064)(0x0a64)(0x3232))
+    ((0x0000)(0x0001)(0x0102))
     ((default_generator)(limited_range))
     ((default_generator)(limited_range))
 )
 UNORDERED_MULTI_TEST(map_merge, merge_exception_test,
     ((test_map_)(test_multimap_))
     ((test_map_)(test_multimap_))
-    ((0)(10)(100))
-    ((0)(10)(100))
-    ((0)(1)(2))
-    ((0)(1)(2))
+    ((0x0000)(0x6400)(0x0064)(0x0a64)(0x3232))
+    ((0x0101)(0x0200)(0x0201))
     ((default_generator)(limited_range))
     ((default_generator)(limited_range))
 )
@@ -86,20 +85,16 @@ UNORDERED_MULTI_TEST(map_merge, merge_exception_test,
 UNORDERED_MULTI_TEST(set_merge_collisions, merge_exception_test,
     ((test_set_)(test_multiset_))
     ((test_set_)(test_multiset_))
-    ((10))
-    ((10))
-    ((0)(1)(2))
-    ((0)(1)(2))
+    ((0x0a0a))
+    ((0x0202)(0x0100)(0x0201))
     ((generate_collisions))
     ((generate_collisions))
 )
 UNORDERED_MULTI_TEST(map_merge_collisions, merge_exception_test,
     ((test_map_)(test_multimap_))
     ((test_map_)(test_multimap_))
-    ((10))
-    ((10))
-    ((0)(1)(2))
-    ((0)(1)(2))
+    ((0x0a0a))
+    ((0x0000)(0x0002)(0x0102))
     ((generate_collisions))
     ((generate_collisions))
 )
