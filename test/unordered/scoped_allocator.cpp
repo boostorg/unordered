ea9af551a62683d71627740ed9303646c6948161
LeonineKing1199 <christian.mazakas@gmail.com>
2021-09-26 08:53:14 -0700

    Refine test to only run in C++11 mode and later with possibility of re-introducing C++03 support later

--- test/unordered/scoped_allocator.cpp
+++ test/unordered/scoped_allocator.cpp
@@ -12,55 +12,66 @@
 
 #include "../helpers/test.hpp"
 
+#include <boost/config.hpp>
+
 #include <boost/container/scoped_allocator.hpp>
+#include <boost/container/uses_allocator.hpp>
+
+#include <boost/core/ignore_unused.hpp>
 
 #include <boost/interprocess/allocators/node_allocator.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 
-#include <cstdint>
 #include <utility>
 #include <vector>
+
+#include <stdint.h>
 
 // This test is based on a user-submitted issue found here:
 // https://github.com/boostorg/unordered/issues/22
 //
 
+namespace bi = boost::interprocess;
+
+template <typename T> struct node_alloc
+{
+  typedef bi::node_allocator<T, bi::managed_shared_memory::segment_manager>
+    type;
+};
+
+typedef std::vector<uint64_t, node_alloc<uint64_t>::type> vector_type;
+
+typedef std::pair<uint64_t, vector_type> pair_type;
+
+typedef boost::container::scoped_allocator_adaptor<node_alloc<pair_type>::type,
+  node_alloc<uint64_t>::type>
+  allocator_type;
+
+typedef boost::unordered_map<const uint64_t, vector_type, boost::hash<uint64_t>,
+  std::equal_to<uint64_t>, allocator_type>
+  map_type;
+
 namespace scoped_allocator {
-  namespace bi = boost::interprocess;
-
-  template <typename T>
-  using scoped_alloc_t = boost::container::scoped_allocator_adaptor<T>;
-
-  template <class T>
-  using alloc_type =
-    bi::node_allocator<T, bi::managed_shared_memory::segment_manager>;
 
   void use_scoped_allocator()
   {
-    using way_type = std::vector<uint64_t, alloc_type<uint64_t> >;
-
-    using waystore_pair_type = std::pair<uint64_t, way_type>;
-
-    using waystore_pair_allocator_type = alloc_type<waystore_pair_type>;
-
-    using map_type =
-      boost::unordered_map<const uint64_t, way_type, std::hash<uint64_t>,
-        std::equal_to<uint64_t>, scoped_alloc_t<waystore_pair_allocator_type> >;
-
+#if BOOST_CXX_VERSION > 199711L
     bi::managed_shared_memory s(
       bi::create_only, "unordered-shared-mem-test", 65536);
 
-    map_type map(s.get_segment_manager());
+    allocator_type alloc(node_alloc<pair_type>::type(s.get_segment_manager()),
+      node_alloc<uint64_t>::type(s.get_segment_manager()));
+
+    map_type map(alloc);
 
     for (unsigned i = 0; i < 10; ++i) {
-      auto const way = {1, 2, 3, 4};
-      map.emplace(std::piecewise_construct, std::forward_as_tuple(i),
-        std::forward_as_tuple(way.begin(), way.end()));
+      boost::ignore_unused(map[i]);
     }
 
     BOOST_TEST(map.size() == 10);
 
     bi::shared_memory_object::remove("unordered-shared-mem-test");
+#endif
   }
 
   UNORDERED_TEST(use_scoped_allocator, (()))
