559824e0da4b34bd2b1f606f6c482060953ec3bc
LeonineKing1199 <christian.mazakas@gmail.com>
2021-10-09 12:05:36 -0700

    Refactor test to use `UNORDERED_AUTO_TEST`

--- test/unordered/scoped_allocator.cpp
+++ test/unordered/scoped_allocator.cpp
@@ -51,31 +51,24 @@ typedef boost::unordered_map<const boost::uint64_t, vector_type,
   boost::hash<boost::uint64_t>, std::equal_to<boost::uint64_t>, allocator_type>
   map_type;
 
-namespace scoped_allocator {
-
-  void use_scoped_allocator()
-  {
+UNORDERED_AUTO_TEST (scoped_allocator) {
 #if BOOST_CXX_VERSION > 199711L
-    bi::managed_shared_memory s(
-      bi::create_only, "unordered-shared-mem-test", 65536);
+  bi::managed_shared_memory s(
+    bi::create_only, "unordered-shared-mem-test", 65536);
 
-    allocator_type alloc(node_alloc<pair_type>::type(s.get_segment_manager()),
-      node_alloc<boost::uint64_t>::type(s.get_segment_manager()));
+  allocator_type alloc(node_alloc<pair_type>::type(s.get_segment_manager()),
+    node_alloc<boost::uint64_t>::type(s.get_segment_manager()));
 
-    map_type map(alloc);
+  map_type map(alloc);
 
-    for (unsigned i = 0; i < 10; ++i) {
-      boost::ignore_unused(map[i]);
-    }
-
-    BOOST_TEST(map.size() == 10);
-
-    bi::shared_memory_object::remove("unordered-shared-mem-test");
-#endif
+  for (unsigned i = 0; i < 10; ++i) {
+    boost::ignore_unused(map[i]);
   }
 
-  UNORDERED_TEST(use_scoped_allocator, (()))
+  BOOST_TEST(map.size() == 10);
 
-} // namespace scoped_allocator
+  bi::shared_memory_object::remove("unordered-shared-mem-test");
+#endif
+}
 
 RUN_TESTS()
