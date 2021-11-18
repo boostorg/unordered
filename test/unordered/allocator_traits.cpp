2e1ef850e3e42108edc7f7aae037a6bf6a41c3e9
LeonineKing1199 <christian.mazakas@gmail.com>
2021-11-18 10:25:44 -0800

    Add const qualification to auto-generated allocator methods in test suite

--- test/unordered/allocator_traits.cpp
+++ test/unordered/allocator_traits.cpp
@@ -65,9 +65,9 @@
   {                                                                            \
     return (std::numeric_limits<size_type>::max)();                            \
   }                                                                            \
-  bool operator==(name<T> const&) { return true; }                             \
-  bool operator!=(name<T> const&) { return false; }                            \
-/**/
+  bool operator==(name<T> const&) const { return true; }                       \
+  bool operator!=(name<T> const&) const { return false; }                      \
+  /**/
 
 struct yes_type
 {
