3df902af23c996fdfc1e746bc537115e6edf98c6
Christian Mazakas <christian.mazakas@gmail.com>
2022-06-30 09:46:37 -0700

    Pull `prime_fmod_size` into its own dedicated header, update #include list for fca.hpp and prime_fmod_test.hpp

--- test/unordered/prime_fmod_tests.cpp
+++ test/unordered/prime_fmod_tests.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/unordered/detail/fca.hpp>
+#include <boost/unordered/detail/prime_fmod.hpp>
 
 #include <boost/core/detail/splitmix64.hpp>
 #include <boost/core/lightweight_test.hpp>
