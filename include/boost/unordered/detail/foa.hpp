63fdf675689edc65f2b74211abcfee0788316cba
joaquintides <joaquin@tid.es>
2022-09-28 20:19:08 +0200

    un-inlined pow2_quadratic_prober memfuns to see it impacts performance

--- include/boost/unordered/detail/foa.hpp
+++ include/boost/unordered/detail/foa.hpp
@@ -408,9 +408,9 @@ struct pow2_quadratic_prober
 {
   pow2_quadratic_prober(std::size_t pos_):pos{pos_}{}
 
-  inline std::size_t get()const{return pos;}
+  std::size_t get()const{return pos;}
 
-  inline bool next(std::size_t mask)
+  bool next(std::size_t mask)
   {
     step+=1;
     pos=(pos+step)&mask;
