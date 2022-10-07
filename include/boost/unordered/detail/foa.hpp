6e8e2112ba0e563e748c0e65de63a40ae9e3419d
Christian Mazakas <christian.mazakas@gmail.com>
2022-10-07 14:51:40 -0700

    Rearrange SFINAE to appease msvc-14.0 in erase() member function template

--- include/boost/unordered/detail/foa.hpp
+++ include/boost/unordered/detail/foa.hpp
@@ -1097,13 +1097,11 @@ public:
     --size_;
   }
 
-  template<
-    typename Key,
-    typename std::enable_if<
-      !std::is_convertible<Key&&,iterator>::value&&
-      !std::is_convertible<Key&&,const_iterator>::value>::type* =nullptr
-  >
-  std::size_t erase(Key&& x)
+  template<typename Key>
+  auto erase(Key&& x) -> typename std::enable_if<
+    !boost::is_convertible<Key,iterator>::value&&
+    !boost::is_convertible<Key,const_iterator>::value, std::size_t>::type
+  erase(Key&& x)
   {
     auto it=find(x);
     if(it!=end()){
