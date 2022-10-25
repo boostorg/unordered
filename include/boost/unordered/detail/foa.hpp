7db310f6d27fbd2b37c5e831f00a515a21cd6b67
joaquintides <joaquin@tid.es>
2022-10-25 11:48:41 +0200

    reverted 0b575dc98304af2b8e8f565783f0f85898df3b52 (may affect performance)

--- include/boost/unordered/detail/foa.hpp
+++ include/boost/unordered/detail/foa.hpp
@@ -387,7 +387,7 @@ private:
 
   static inline int simde_mm_movemask_epi8(uint8x16_t a)
   {
-    static constexpr uint8_t md[16]={
+    static const uint8_t md[16]={
       1 << 0, 1 << 1, 1 << 2, 1 << 3,
       1 << 4, 1 << 5, 1 << 6, 1 << 7,
       1 << 0, 1 << 1, 1 << 2, 1 << 3,
