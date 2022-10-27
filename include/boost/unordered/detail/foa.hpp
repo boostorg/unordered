231f9785ff892215a23f186cbc134df64406bf64
joaquintides <joaquin@tid.es>
2022-10-27 18:56:18 +0200

    reverted

--- include/boost/unordered/detail/foa.hpp
+++ include/boost/unordered/detail/foa.hpp
@@ -1559,8 +1559,7 @@ private:
         prefetch_elements(p);
         do{
           auto n=unchecked_countr_zero(mask);
-          //if(BOOST_LIKELY(bool(pred()(x,key_from(p[n]))))){
-          if(BOOST_LIKELY(bool(pred()(x,p[n].first)))){
+          if(BOOST_LIKELY(bool(pred()(x,key_from(p[n]))))){
             return {pg,n,p+n};
           }
           mask&=mask-1;
