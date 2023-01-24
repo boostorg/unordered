96027997106cb321977a9686c69c440f418ca119
Christian Mazakas <christian.mazakas@gmail.com>
2023-01-24 15:27:52 -0800

    Add node-based foa containers to link tests

--- test/unordered/link_test_1.cpp
+++ test/unordered/link_test_1.cpp
@@ -1,20 +1,23 @@
 
 // Copyright 2006-2009 Daniel James.
-// Copyright 2022 Christian Mazakas.
+// Copyright 2022-2023 Christian Mazakas.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include "../helpers/unordered.hpp"
 
 #ifdef BOOST_UNORDERED_FOA_TESTS
-void foo(boost::unordered_flat_set<int>&, boost::unordered_flat_map<int, int>&);
+void foo(boost::unordered_flat_set<int>&, boost::unordered_flat_map<int, int>&,
+  boost::unordered_node_set<int>&, boost::unordered_node_map<int, int>&);
 
 int main()
 {
   boost::unordered_flat_set<int> x1;
   boost::unordered_flat_map<int, int> x2;
+  boost::unordered_node_set<int> x3;
+  boost::unordered_node_map<int, int> x4;
 
-  foo(x1, x2);
+  foo(x1, x2, x3, x4);
 
   return 0;
 }
