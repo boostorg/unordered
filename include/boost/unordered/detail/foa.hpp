b08810c0c4bd8490c8292e024ed5b59357654738
Christian Mazakas <christian.mazakas@gmail.com>
2023-02-19 21:08:00 -0800

    Add missing copyright notice to foa.hpp

--- include/boost/unordered/detail/foa.hpp
+++ include/boost/unordered/detail/foa.hpp
@@ -1,6 +1,7 @@
 /* Fast open-addressing hash table.
  *
  * Copyright 2022-2023 Joaquin M Lopez Munoz.
+ * Copyright 2023 Christian Mazakas.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
