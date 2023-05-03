3c0fb0fa1bb8a16225c2cf9753c42484950d0527
Christian Mazakas <christian.mazakas@gmail.com>
2023-05-03 11:22:07 -0700

    Attempt to fix flaky CI

--- test/cfoa/swap_tests.cpp
+++ test/cfoa/swap_tests.cpp
@@ -191,6 +191,7 @@ namespace {
           if (idx % 100 == 0) {
             cv.notify_all();
           }
+          std::this_thread::yield();
         }
 
         done1 = true;
@@ -201,6 +202,7 @@ namespace {
 
         for (auto const& val : vals2) {
           x2.insert(val);
+          std::this_thread::yield();
         }
 
         done2 = true;
@@ -214,7 +216,11 @@ namespace {
           }
           x1.swap(x2);
           ++num_swaps;
+          std::this_thread::yield();
         } while (!done1 || !done2);
+
+        BOOST_TEST(done1);
+        BOOST_TEST(done2);
       });
 
       t1.join();
