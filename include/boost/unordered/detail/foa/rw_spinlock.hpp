8877d2123736e71e6838a210a8c8f569614d35fe
Christian Mazakas <christian.mazakas@gmail.com>
2023-06-02 14:14:18 -0700

    Replace dependency on SmartPtr with primitives in Core

--- include/boost/unordered/detail/foa/rw_spinlock.hpp
+++ include/boost/unordered/detail/foa/rw_spinlock.hpp
@@ -5,8 +5,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/smart_ptr/detail/sp_thread_pause.hpp>
-#include <boost/smart_ptr/detail/sp_thread_sleep.hpp>
+#include <boost/core/yield_primitives.hpp>
 #include <atomic>
 #include <cstdint>
 
@@ -64,10 +63,10 @@ public:
                     if( state_.compare_exchange_weak( st, newst, std::memory_order_acquire, std::memory_order_relaxed ) ) return;
                 }
 
-                boost::detail::sp_thread_pause();
+                boost::core::sp_thread_pause();
             }
 
-            boost::detail::sp_thread_sleep();
+            boost::core::sp_thread_sleep();
         }
     }
 
@@ -132,7 +131,7 @@ public:
                     state_.compare_exchange_weak( st, newst, std::memory_order_relaxed, std::memory_order_relaxed );
                 }
 
-                boost::detail::sp_thread_pause();
+                boost::core::sp_thread_pause();
             }
 
             // clear writer pending bit before going to sleep
@@ -169,7 +168,7 @@ public:
                 }
             }
 
-            boost::detail::sp_thread_sleep();
+            boost::core::sp_thread_sleep();
         }
     }
 
