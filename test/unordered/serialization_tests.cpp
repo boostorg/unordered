cf298cba7fdd33e5270eff01f99a5e3908813231
joaquintides <joaquin@tid.es>
2023-08-15 15:57:52 +0200

    removed spurious macro definition

--- test/unordered/serialization_tests.cpp
+++ test/unordered/serialization_tests.cpp
@@ -2,7 +2,6 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#define BOOST_UNORDERED_FOA_TESTS
 #include "../helpers/unordered.hpp"
 
 #include "../objects/test.hpp"
