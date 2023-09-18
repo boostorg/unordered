c0cd51bd2a3446556dd550f3337dbbe7b3e787ef
Christian Mazakas <christian.mazakas@gmail.com>
2023-09-18 11:45:35 -0700

    Update test jamfile to glob for project headers and create self-include tests

---
+++ test/unordered/self_include_tests.cpp
@@ -0,0 +1,11 @@
+#ifndef BOOST_UNORDERED_HEADER
+#error "this requires a class template be passed as a macro"
+#endif
+
+#define STRINGIZE(text) STRINGIZE_I(text)
+#define STRINGIZE_I(...) #__VA_ARGS__
+
+#define HEADER STRINGIZE(BOOST_UNORDERED_HEADER)
+#include HEADER
+
+int main() {}
