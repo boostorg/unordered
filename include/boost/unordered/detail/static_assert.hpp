a9da328e1d5b5e8a70f9980c73e354d1b970e6e6
Christian Mazakas <christian.mazakas@gmail.com>
2023-09-27 12:55:27 -0700

    Add missing copyright to static_assert() header

--- include/boost/unordered/detail/static_assert.hpp
+++ include/boost/unordered/detail/static_assert.hpp
@@ -1,3 +1,7 @@
+// Copyright 2023 Christian Mazakas
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef BOOST_UNORDERED_DETAIL_STATIC_ASSERT_HPP
 #define BOOST_UNORDERED_DETAIL_STATIC_ASSERT_HPP
 
