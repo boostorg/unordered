145490a370854cf95f2edd80435ddc43e31488a8
joaquintides <joaquin@tid.es>
2025-04-11 19:27:50 +0200

    added pull to open-addressing containers

--- include/boost/unordered/detail/foa/table.hpp
+++ include/boost/unordered/detail/foa/table.hpp
@@ -1,6 +1,6 @@
 /* Fast open-addressing hash table.
  *
- * Copyright 2022-2024 Joaquin M Lopez Munoz.
+ * Copyright 2022-2025 Joaquin M Lopez Munoz.
  * Copyright 2023 Christian Mazakas.
  * Copyright 2024 Braden Ganetsky.
  * Distributed under the Boost Software License, Version 1.0.
@@ -493,6 +493,14 @@ public:
       return 1;
     }
     else return 0;
+  }
+
+  BOOST_FORCEINLINE init_type pull(const_iterator pos)
+  {
+    BOOST_ASSERT(pos!=end());
+    init_type res(type_policy::move(type_policy::value_from(*pos.p())));
+    super::erase(pos.pc(),pos.p());
+    return res;
   }
 
   void swap(table& x)
